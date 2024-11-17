a2d75c145cfa4bbcaf6281d3969703732e9266a1
Yuki Kiriha <87764627+reineowo@users.noreply.github.com>
2024-11-18 04:51:06 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# api-random-image
+# image-random-api
+
+## Setup
+
+#### 1. Clone the repository.
+```
+git clone https://github.com/HELLSNAKES/image-random-api.git
+```
+#### 2. Navigate to the directory.
+```
+cd image-random-api
+```
+#### 3. Install the dependencies.
+```
+npm i
+```
+#### 4. Start the application.
+```
+npm start
+```
+#### 5. Open http://127.0.1.1:5000/kurumi?apikey=0bk3s6IAyq
+> Note : 0bk3s6IAyq is api key
+
+* endpoit 
+- /kurumi?apikey=
+- /rushia?apikey=
+
+## Video Installation instructions
+
+* Replit
+ [![a](https://i.ytimg.com/vi/8grd5gOyX7I/maxresdefault.jpg)](https://www.youtube.com/watch?v=8grd5gOyX7I)
---
+++ server.js
@@ -0,0 +1,72 @@
+import express from "express"
+import fs from "fs"
+import cors from "cors"
+import rateLimit from "express-rate-limit"
+const port = process.env.PORT || 5000
+const app = express()
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+})
+app.use(express.static("public"))
+app.use(limiter)
+app.use(cors())
+// api key
+const apikey = [
+
+    "mr8es2r0hK",
+    "0bk3s6IAyq",
+    "T6j6vW01A2",
+    "mr83ssr0hK",
+    "mr8e2r0hK",
+    "mr8es2r0hK",
+    "mr8esar0hK",
+    "mr8esgr0hK",
+    "mr8esar0hK"
+]
+app.get("/", (req, res) => {
+    res.send("https://github.com/reineowo/api-random-image")
+})
+app.get("/kurumi", (req, res) => {
+    const key = req.query.apikey
+    const result = {}
+    result.code = 200
+    const imageList = fs.readdirSync("./public/kurumi")
+    const randomImage = imageList[Math.floor(Math.random() * imageList.length)]
+    result.url = `127.0.0.1:5000/kurumi/${randomImage}`
+    result.author = "HELLSNAKE , Sunglows Team"
+    result.source = "https://github.com/reineowo/api-random-image"
+    res.header("Content-type", "application/json; charset=utf-8")
+    if (apikey.includes(key)) {
+        res.send(JSON.stringify(result, null, 2))
+        console.log(result)
+    } else {
+        const result = {}
+        result.code = 403
+        result.message = "Invalid API key, please contact admin to get key"
+        res.send(JSON.stringify(result, null, 2))
+    }
+})
+app.get("/rushia", (req, res) => {
+    const key = req.query.apikey
+    const result = {}
+    result.code = 200
+    const imageList = fs.readdirSync("./public/rushia")
+    const randomImage = imageList[Math.floor(Math.random() * imageList.length)]
+    result.url = `127.0.0.1:5000/rushia/${randomImage}`
+    result.author = "HELLSNAKE , Sunglows Team"
+    result.source = "https://github.com/reineowo/api-random-image"
+    res.header("Content-type", "application/json; charset=utf-8")
+    if (apikey.includes(key)) {
+        res.send(JSON.stringify(result, null, 2))
+        console.log(result)
+    } else {
+        const result = {}
+        result.code = 403
+        result.message = "Invalid API key, please contact admin to get key"
+        res.send(JSON.stringify(result, null, 2))
+    }
+})
+app.listen(port, "0.0.0.0", function () {
+    console.log(`Server listening on port ${port}\n`)
+})
